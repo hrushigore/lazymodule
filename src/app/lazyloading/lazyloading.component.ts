878966fe0cde680f9c64e56ac40c8885700b8eff
Hrushi <hrushikeshgore7@gmail.com>
2018-04-25 22:54:54 +0530

    Modify ts file and index.html

--- src/app/lazyloading/lazyloading.component.ts
+++ src/app/lazyloading/lazyloading.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lazyloading',
   templateUrl: './lazyloading.component.html',
-  styleUrls: ['./lazyloading.component.css',
-  '../../assets/user/css/bootstrap.min.css',
-  '../../assets/user/css/style.css',
-  '../../assets/user/plugins/owl-carousel/owl.carousel.css',
-  '../../assets/user/plugins/owl-carousel/owl.theme.css',
-  '../../assets/user/font-awesome/css/font-awesome.min.css'
+  styleUrls: ['./lazyloading.component.css'
+  // '../../assets/user/css/bootstrap.min.css',
+  // '../../assets/user/css/style.css',
+  // '../../assets/user/plugins/owl-carousel/owl.carousel.css',
+  // '../../assets/user/plugins/owl-carousel/owl.theme.css',
+  // '../../assets/user/font-awesome/css/font-awesome.min.css'
 ]
 })
 export class LazyloadingComponent implements OnInit {
